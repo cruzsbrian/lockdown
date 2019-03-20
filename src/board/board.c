b5ed534a3b5d6a8ab34ce9e505b2490c012cd3b2
Brian Cruz <cruz.s.brian@gmail.com>
2019-03-19 17:23:19 -0700

    Board allocation, adding pieces, pretty printing

--- src/board/board.c
+++ src/board/board.c
@@ -1,3 +1,53 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "board.h"
+
+
+board_t *create_board(void) {
+    /* Use calloc to zero out memory */
+    board_t *new_board = (board_t *)calloc(1, sizeof(board_t));
+    
+    if (new_board == NULL) {
+        fprintf(stderr, "Error: Unable to allocate new board.");
+        exit(1);
+    }
+
+    return new_board;
+}
+
+
+
+
+void add_piece(board_t *board, int pos, int color) {
+    if (color == BLACK) {
+        board->b |= (1L << pos);
+    } else {
+        board->w |= (1L << pos);
+    }
+}
+
+
+
+
+void print_board(board_t *board) {
+    int pos, b, w;
+
+    int ii, jj;
+    for (ii = 0; ii < 8; ++ii) {
+        for (jj = 0; jj < 8; ++jj) {
+            pos = (ii * 8) + jj;
+            b = (board->b >> pos) & 1L;
+            w = (board->w >> pos) & 1L;
+
+            if (w) {
+                printf("W ");
+            } else if (b) {
+                printf("B ");
+            } else {
+                printf("_ ");
+            }
+        }
+        printf("\n");
+    }
+}
--- src/board/board.h
+++ src/board/board.h
@@ -1,4 +1,30 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include <stdint.h>
+
+#define BLACK 0
+#define WHITE 1
+
+
+typedef struct {
+    uint64_t b;
+    uint64_t w;
+} board_t;
+
+
+board_t *create_board(void);
+board_t *copy_board(board_t *old);
+
+
+int popcount(uint64_t bits);
+int popcount_board(board_t *board);
+
+
+void add_piece(board_t *board, int pos, int color);
+
+
+void print_board(board_t *board);
+
+
 #endif
